f4f8425fc7f385ac9b6a9e7bb04a74be2049d2f7
ruchika6698 <ruchika6698@gmail.com>
2020-09-21 20:05:33 +0530

    Added:User Controller for User API's

---
+++ ParkingLotAPI/Controllers/UserController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Interface;
+using CommonLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ParkingLotAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IConfiguration _config;
+        IUserBusinessLayer BusinessLayer;
+
+        public UserController(IUserBusinessLayer BusinessDependencyInjection, IConfiguration config)
+        {
+            BusinessLayer = BusinessDependencyInjection;
+            _config = config;
+        }
+
+        /// <summary>
+        ///  API for Registrion
+        /// </summary>
+        /// <param name="Info"> store the Complete Employee information</param>
+        /// <returns></returns>
+        [Route("register")]
+        [HttpPost]
+        public async Task<IActionResult> UserRegister([FromBody] Usermodel Info)
+        {
+            try
+            {
+                bool data = await BusinessLayer.UserRegister(Info);
+                //if data is not equal to null then Registration sucessful
+                if (!data.Equals(null))
+                {
+                    var status = "Success";
+                    var Message = "Registration Successfull";
+                    return this.Ok(new { status, Message, Info });
+                }
+                else                                     //Registration Fail
+                {
+                    var status = "Unsuccess";
+                    var Message = "Registration Failed";
+                    return this.BadRequest(new { status, Message, data = Info });
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { error = e.Message });
+            }
+        }
+
+        /// <summary>
+        ///  API for Login
+        /// </summary>
+        /// <param name="Info"> Login API</param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> UserLogin([FromBody] Login Info)
+        {
+            try
+            {
+                int Result = await BusinessLayer.UserLogin(Info);
+                var symmetricSecuritykey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+
+                var signingCreds = new SigningCredentials(symmetricSecuritykey, SecurityAlgorithms.HmacSha256);
+
+                var claims = new List<Claim>
+                {
+                    new Claim("Username", Info.Username.ToString()),
+                    new Claim("Password", Info.Password.ToString())
+                };
+                var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+                    _config["Jwt:Issuer"],
+                    claims,
+                    expires: DateTime.Now.AddHours(120),
+                    signingCredentials: signingCreds);
+                //if Result is not equal to null then Login sucessful
+                if (Result != 0)
+                {
+                    var status = "Success";
+                    var Message = "Login Successful";
+                    var Token = new JwtSecurityTokenHandler().WriteToken(token);
+                    return Ok(new { status, Message, Token });
+                }
+                else                                        //Username or Password Incorrect
+                {
+                    var status = "Unsuccess";
+                    var Message = "Invaid Username Or Password";
+                    return BadRequest(new { status, Message, Result = Info });
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
