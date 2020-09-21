646c65d8303e88725ed3a9c0ba51e7ed88e0e284
ruchika6698 <ruchika6698@gmail.com>
2020-09-21 20:06:53 +0530

    Added:Business logic for Register and login

---
+++ BusinessLayer/Services/UserBusinessLayer.cs
@@ -0,0 +1,69 @@
+﻿using BusinessLayer.Interface;
+using CommonLayer;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services
+{
+    public class UserBusinessLayer : IUserBusinessLayer
+    {
+        private IUserRepositoryLayer _UserRepository;
+        public UserBusinessLayer(IUserRepositoryLayer UserRepository)
+        {
+            _UserRepository = UserRepository;
+        }
+        /// <summary>
+        ///  API for Registrion
+        /// </summary>
+        /// <param name="data"> store the Complete Employee information</param>
+        /// <returns></returns>
+        public async Task<bool> UserRegister(Usermodel data)
+        {
+            try
+            {
+                var Result = await _UserRepository.UserRegister(data);
+                //if result is not equal null then return true
+                if (!Result.Equals(null))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+        /// <summary>
+        ///  API for Login
+        /// </summary>
+        /// <param name="data"> Login API</param>
+        /// <returns></returns>
+        public async Task<int> UserLogin(Login data)
+        {
+            try
+            {
+                var Result = await _UserRepository.UserLogin(data);
+                //if result is not equal null then return true
+                if (!Result.Equals(0))
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
