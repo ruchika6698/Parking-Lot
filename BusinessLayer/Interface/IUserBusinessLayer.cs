a75a5a02b419412a18b2672237fa11982d4c91e9
ruchika6698 <ruchika6698@gmail.com>
2020-09-21 20:08:59 +0530

    Added:Business Layer logic for register and login

---
+++ BusinessLayer/Interface/IUserBusinessLayer.cs
@@ -0,0 +1,22 @@
+﻿///-----------------------------------------------------------------
+///   Class:       IUserBusinessLayer
+///   Description: Business Layer Interface for User
+///   Author:      Ruchika                   Date: 21/9/2020
+///-----------------------------------------------------------------
+using CommonLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Interface
+{
+    public interface IUserBusinessLayer
+    {
+        //Interface method for User Registration
+        Task<bool> UserRegister(Usermodel data);
+
+        //Interface method for User Login
+        Task<int> UserLogin(Login data);
+    }
+}
