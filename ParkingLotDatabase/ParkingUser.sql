bd896b89d7a6e43e6c712fec1522d5da71725adb
ruchika6698 <ruchika6698@gmail.com>
2020-09-22 10:59:56 +0530

    Refactor:Store procedures for user table

--- ParkingLotDatabase/ParkingUser.sql
+++ ParkingLotDatabase/ParkingUser.sql
@@ -4,13 +4,8 @@ CREATE TABLE ParkingUser
   FirstName varchar(50),
   LastName varchar(50),
   EmailID varchar(50),
-  DriverCategory varchar(50),
-  Username varchar(50),
   Password varchar(50),
-  VehicalNumber  varchar(50),
-  VehicalBrand  varchar(50),
-  ParkingType  varchar(50),
-  CreateDate varchar(50),
-  ModifiedDate varchar(50)
+  DriverCategory varchar(50),
+  CreateDate varchar(50)
 );
 select * from ParkingUser
